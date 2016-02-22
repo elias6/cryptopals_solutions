94a770db6705251564a7a61787a1ec9976f3716b
Elias Zamaria <mikez302@gmail.com>
2016-02-21 19:54:22 -0800

    Add assert to challenge 1

--- challenges.py
+++ challenges.py
@@ -77,7 +77,9 @@ def best_english_like_score_data(text, num=1):
 def challenge1():
     cipher_hex = ("49276d206b696c6c696e6720796f757220627261696e206c" +
         "696b65206120706f69736f6e6f7573206d757368726f6f6d")
-    print(bytes_to_string(hex_to_base64(cipher_hex)))
+    result = hex_to_base64(cipher_hex)
+    assert result == b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    print(bytes_to_string(result))
 
 def challenge2():
     output = xor_bytes(
