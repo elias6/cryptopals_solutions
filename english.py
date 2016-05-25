85ceed751ddd2fc683e5803516ef3871a0d0aa1d
Elias Zamaria <mikez302@gmail.com>
2016-05-25 11:16:47 -0700

    Add all_bytes_by_frequency sequence to english module

--- english.py
+++ english.py
@@ -1,6 +1,52 @@
 from collections import defaultdict
+from itertools import chain
 
 from util import xor_encrypt
+
+# All bytes, from most common to least common in UTF-8-encoded English
+# text. Data for this sequence is mostly taken from
+# http://www.indiana.edu/~clcl/Papers/LFE.pdf. The paper does not
+# mention certain characters, so I used my judgment as to where to put
+# them, trying to come up with something that produces good results.
+all_bytes_by_frequency = bytes(chain(
+    b" ",    # Space, not mentioned in paper.
+
+    # Characters in paper, from most common to least common.
+    b"etaonisrhldcumfpgyw,.bv01k52TS\"9AM-CIN'438"
+    b"B6RPEDHx7WLOFYGJzjUq:)($K;V*?Q/X&Z!%+><=#@",
+
+    b"\n[\\]^_`{|}~\t\r",    # ASCII characters not mentioned in paper.
+
+    # Continuation bytes in multi-byte characters (code points 0x80 -
+    # 0x1fffff).
+    range(0x80, 0xc0),
+
+    # Leading byte of 2-byte characters (code points 0x80 - 0x7ff) that
+    # occasionally appear in English text.
+    range(0xc2, 0xe0),
+
+    # Leading byte of 3-byte characters (code points 0x800 - 0xffff) that
+    # rarely appear in English text.
+    range(0xe0, 0xf0),
+
+    # Leading byte of 4-byte characters (code points 0x10000 - 0x1fffff)
+    # that rarely appear in English text.
+    range(0xf0, 0xf5),
+
+    # Control codes rarely used in practice.
+    (x for x in range(0x00, 0x20) if bytes([x]) not in b"\n\t\r"),
+
+    b"\x7f",    # Delete character rarely used in practice.
+
+    range(0xc0, 0xc2),    # Overlong encoding, not valid UTF-8.
+
+    range(0xf5, 0xf8),    # Overlong encoding, not valid UTF-8.
+
+    range(0xf8, 0x100)    # Not valid UTF-8.
+))
+
+assert sorted(all_bytes_by_frequency) == list(range(256))
+
 
 # Character frequencies were taken from raw letter averages at
 # http://www.macfreek.nl/memory/Letter_Distribution, then rounded to 6
