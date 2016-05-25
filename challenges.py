elias6/cryptopals_solutions

commit 51bb1927405c0c96a52686b8ff77d18e861a2661

Minor changes related to IETF prime

challenges.py
@@ -33,9 +33,8 @@ from block_cipher import (crack_ecb_oracle, ctr_counter, ctr_iterator, guess_blo
 from mersenne_twister import MT19937_RNG
 from timing_server import (FancyHTTPServer, ValidatingRequestHandler, insecure_compare,
     recover_signature, server_approves_of_signature)
-from util import (IETF_DIFFIE_HELLMAN_PRIME, big_int_cube_root, chunks, gcd, get_hmac,
-    int_to_bytes, invmod, pkcs7_pad, pkcs7_unpad, pprint, random, sha1, xor_bytes,
-    xor_encrypt)
+from util import (IETF_PRIME, big_int_cube_root, chunks, gcd, get_hmac, int_to_bytes,
+    invmod, pkcs7_pad, pkcs7_unpad, pprint, random, sha1, xor_bytes, xor_encrypt)
 
 warnings.simplefilter("default", BytesWarning)
 warnings.simplefilter("default", ResourceWarning)
@@ -823,10 +822,10 @@ def challenge35():
     mallory.send_echo_request(bob, EXAMPLE_PLAIN_BYTES)
     assert EXAMPLE_PLAIN_BYTES in bob._decrypted_messages[mallory]
 
-    # Mallory tricks Alice and Bob into using g=IETF_DIFFIE_HELLMAN_PRIME
-    alice = diffie_hellman.User(g=IETF_DIFFIE_HELLMAN_PRIME)
-    bob = diffie_hellman.User(g=IETF_DIFFIE_HELLMAN_PRIME)
-    mallory = diffie_hellman.User(g=IETF_DIFFIE_HELLMAN_PRIME)
+    # Mallory tricks Alice and Bob into using g=IETF_PRIME
+    alice = diffie_hellman.User(g=IETF_PRIME)
+    bob = diffie_hellman.User(g=IETF_PRIME)
+    mallory = diffie_hellman.User(g=IETF_PRIME)
     assert mallory.public_key == 0
     assert alice.get_shared_key_for(mallory) == 0
     assert bob.get_shared_key_for(mallory) == 0
@@ -836,12 +835,12 @@ def challenge35():
     mallory.send_echo_request(bob, EXAMPLE_PLAIN_BYTES)
     assert EXAMPLE_PLAIN_BYTES in bob._decrypted_messages[mallory]
 
-    # Mallory tricks Alice and Bob into using g=IETF_DIFFIE_HELLMAN_PRIME - 1
-    alice = diffie_hellman.User(g=IETF_DIFFIE_HELLMAN_PRIME - 1)
-    bob = diffie_hellman.User(g=IETF_DIFFIE_HELLMAN_PRIME - 1)
+    # Mallory tricks Alice and Bob into using g=IETF_PRIME - 1
+    alice = diffie_hellman.User(g=IETF_PRIME - 1)
+    bob = diffie_hellman.User(g=IETF_PRIME - 1)
     # Private key must be even.
-    mallory = diffie_hellman.User(g=IETF_DIFFIE_HELLMAN_PRIME - 1,
-        private_key=random.randrange(0, IETF_DIFFIE_HELLMAN_PRIME, 2))
+    mallory = diffie_hellman.User(g=IETF_PRIME - 1,
+        private_key=random.randrange(0, IETF_PRIME, 2))
     assert mallory.public_key == 1
     assert alice.get_shared_key_for(mallory) == 1
     assert bob.get_shared_key_for(mallory) == 1

diffie_hellman.py
@@ -3,11 +3,10 @@ from os import urandom
 
 from Crypto.Cipher import AES
 
-from util import (IETF_DIFFIE_HELLMAN_PRIME, int_to_bytes, pkcs7_pad, pkcs7_unpad, random,
-    sha1)
+from util import IETF_PRIME, int_to_bytes, pkcs7_pad, pkcs7_unpad, random, sha1
 
 class User:
-    def __init__(self, p=IETF_DIFFIE_HELLMAN_PRIME, g=2, private_key=None):
+    def __init__(self, p=IETF_PRIME, g=2, private_key=None):
         self.p = p
         if private_key is None:
             self._private_key = random.randint(1, p - 1)

srp.py
@@ -1,10 +1,9 @@
 from hashlib import sha256
 from os import urandom
 
-from util import IETF_DIFFIE_HELLMAN_PRIME, get_hmac, int_to_bytes, random
+from util import IETF_PRIME, get_hmac, int_to_bytes, random
 
 class Server:
-    N = IETF_DIFFIE_HELLMAN_PRIME
     g = 2
 
     def __init__(self):
@@ -16,13 +15,13 @@ class Server:
     def _respond_to_login_request(self, username, A, k=3):
         # A == public ephemeral number from client
         user = self.users[username]
-        b = random.randint(1, self.N - 1)  # private ephemeral number
+        b = random.randint(1, IETF_PRIME - 1)  # private ephemeral number
         # B == public ephemeral number. Usually, B depends on the password, but
         # if k == 0, it is a completely random Diffie-Hellman public key, which
         # causes u to be essentially random.
-        B = ((k * user["verifier"]) + pow(self.g, b, self.N)) % self.N
+        B = ((k * user["verifier"]) + pow(self.g, b, IETF_PRIME)) % IETF_PRIME
         u = scramble_keys(A, B)
-        S = pow(A * pow(user["verifier"], u, self.N), b, self.N)
+        S = pow(A * pow(user["verifier"], u, IETF_PRIME), b, IETF_PRIME)
         user["shared_session_key"] = sha256(int_to_bytes(S)).digest()
         return (user["salt"], B, u)
 
@@ -40,10 +39,10 @@ class MitmServer(Server):
         if k != 0:
             raise ValueError("k must be 0")
         salt, _, _ = self.real_server._respond_to_login_request(username, A, k=k)
-        b = random.randint(1, self.N - 1)
+        b = random.randint(1, IETF_PRIME - 1)
         self.users[username] = {"salt": salt, "A": A, "b": b}
         u = 1
-        B = pow(self.g, b, self.N)
+        B = pow(self.g, b, IETF_PRIME)
         return (salt, B, u)
 
     def _verify_hmac(self, hmac, username):
@@ -55,8 +54,8 @@ class MitmServer(Server):
         u = 1
         for test_password in common_passwords:
             test_x = generate_private_key(username, test_password, user["salt"])
-            test_verifier = pow(self.g, test_x, self.N)
-            test_S = pow(user["A"] * pow(test_verifier, u, self.N), user["b"], self.N)
+            test_verifier = pow(self.g, test_x, IETF_PRIME)
+            test_S = pow(user["A"] * pow(test_verifier, u, IETF_PRIME), user["b"], IETF_PRIME)
             test_session_key = sha256(int_to_bytes(test_S)).digest()
             if get_hmac(test_session_key, user["salt"], sha256) == hmac:
                 user["password"] = test_password
@@ -65,18 +64,17 @@ class MitmServer(Server):
 
 
 class Client:
-    N = IETF_DIFFIE_HELLMAN_PRIME
     g = 2
 
     def sign_up(self, server, username, password):
         salt = urandom(16)
         x = generate_private_key(username, password, salt)
-        verifier = pow(self.g, x, self.N)
+        verifier = pow(self.g, x, IETF_PRIME)
         server._respond_to_sign_up_request(username, salt, verifier)
 
     def log_in(self, server, username, password, k=3):
-        a = random.randint(1, self.N - 1)  # private ephemeral number
-        A = pow(self.g, a, self.N)  # public ephemeral number
+        a = random.randint(1, IETF_PRIME - 1)  # private ephemeral number
+        A = pow(self.g, a, IETF_PRIME)  # public ephemeral number
         # B == public ephemeral number from server
         salt, B, u = server._respond_to_login_request(username, A, k=k)
 
@@ -84,7 +82,7 @@ class Client:
         # the following line is uncommented
         # assert u == scramble_keys(A, B)
         x = generate_private_key(username, password, salt)
-        S = pow(B - k * pow(self.g, x, self.N), a + u*x, self.N)
+        S = pow(B - k * pow(self.g, x, IETF_PRIME), a + u*x, IETF_PRIME)
         shared_session_key = sha256(int_to_bytes(S)).digest()  # called "K" in challenge
         hmac = get_hmac(shared_session_key, salt, sha256)
         return server._verify_hmac(hmac, username)

util.py
@@ -17,13 +17,14 @@ except ImportError:
 random = SystemRandom()
 
 # prime number specified for 1536-bit modular exponential group in RFC
-# at https://datatracker.ietf.org/doc/rfc3526/?include_text=1
-IETF_DIFFIE_HELLMAN_PRIME = int("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1c"
-    "d129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6d"
-    "f25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee"
-    "386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55"
-    "d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c35"
-    "4e4abc9804f1746c08ca237327ffffffffffffffff", 16)
+# at https://datatracker.ietf.org/doc/rfc3526/?include_text=1. Used for
+# Diffie-Hellman and SRP.
+IETF_PRIME = int("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67c"
+    "c74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356"
+    "d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5a"
+    "e9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd2"
+    "4cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746"
+    "c08ca237327ffffffffffffffff", 16)
 
 
 def pprint(*args, width=120, **kwargs):

commit 8896e9bd09c2d09f715a9151274c57f67ae7dc6f

Fix IETF thing in challenge 37
I think I broke it in 51bb1927405c0c96a52686b8ff77d18e861a2661.

challenges.py
@@ -889,7 +889,7 @@ def challenge37():
 
     for i in range(10):
         # Attacker tricks server into computing easily derivable session key
-        salt, _, _ = server._respond_to_login_request(username, i * client.N)
+        salt, _, _ = server._respond_to_login_request(username, i * IETF_PRIME)
         # Attacker derives shared session key without password
         shared_session_key = sha256(int_to_bytes(0)).digest()
         hmac = get_hmac(shared_session_key, salt, sha256)