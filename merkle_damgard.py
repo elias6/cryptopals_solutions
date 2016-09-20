e27c19cb18919f3e627b30f57cf5b378922e90a7
Elias Zamaria <mikez302@gmail.com>
2016-09-20 11:27:42 -0700

    Major speed and reliability improvements to Merkle-Damgård hash function
    It is much faster, and it doesn’t seem to have accidental collisions.

--- merkle_damgard.py
+++ merkle_damgard.py
@@ -1,16 +1,18 @@
+from hashlib import sha512
 from itertools import cycle, islice
 from math import inf
 from os import urandom
 
 import struct
 
-from block_tools import aes_encrypt
 from util import chunks
 
 class HashFunction:
     byte_pattern = bytes.fromhex("0123456789abcdef")
 
     def __init__(self, digest_size, block_size=16):
+        if digest_size > 64:
+            raise ValueError("digest_size must be 64 or less")
         self.digest_size = digest_size
         self.block_size = block_size
         self.initial_state = bytes(islice(cycle(self.byte_pattern), digest_size))
@@ -20,8 +22,7 @@ class HashFunction:
             raise ValueError("length of state must equal digest_size")
         if len(block) != self.block_size:
             raise ValueError("length of block must equal block_size")
-        key = iv = b"\x00"*16
-        return aes_encrypt(state + block, key, "CBC", iv, pad=True)[:self.digest_size]
+        return sha512(state + block).digest()[:self.digest_size]
 
     def padding(self, message_length):
         length_repr = struct.pack(">Q", message_length * 8)
@@ -33,6 +34,7 @@ class HashFunction:
     def __call__(self, message, state=None, *, pad=True):
         state = state or self.initial_state
         prepared_message = message + (self.padding(len(message)) if pad else b"")
+        assert len(prepared_message) % self.block_size == 0
         for block in chunks(prepared_message, self.block_size):
             state = self.compress(state, block)
         return state
